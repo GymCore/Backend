a8948b19bef2c02c417e71e75a3a5db4a432e7d9
Blazej Ciepiel <u31_blacie_lbn@technischools.com>
2025-11-28 13:12:23 +0100

    installed express app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "GymCore API działa" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
